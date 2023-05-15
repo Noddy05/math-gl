b8eac1f8cb4eeb2ba46ab4b03b1fc43d405fb5d0
Noah Dirksen <noah05dirksen@gmail.com>
2023-05-15 20:16:30 +0200

    animations not working yet
    to be finished

---
+++ Animations/Animation.cs
@@ -0,0 +1,48 @@
+﻿using OpenTK.Windowing.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathGL.Animations
+{
+
+    class Animation
+    {
+        struct Function
+        {
+            public float time;
+            public float startValue;
+            public float endValue;
+            public VariableVoid function;
+        }
+
+        private Function function;
+        public virtual void Play() {
+            Program.GetWindow().RenderFrame += OnRender;
+        }
+
+        public delegate void VariableVoid(float t);
+        public delegate float InterpolationMethod(float t);
+        protected void LinearLerp(VariableVoid function, float startValue, float endValue, float time)
+        {
+
+        }
+
+        private float timeSinceStart = 0;
+        private void OnRender(FrameEventArgs args)
+        {
+            timeSinceStart += (float)args.Time;
+            if (timeSinceStart > function.time)
+                Program.GetWindow().RenderFrame -= OnRender;
+
+            Frame(timeSinceStart);
+        }
+
+        public virtual void Frame(float time)
+        {
+
+        }
+    }
+}
---
+++ Animations/SequenceManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathGL.Animations
+{
+    class SequenceManager
+    {
+        public static List<Animation> animations = new();
+
+        private static int queueIndex = 0;
+        public static void PlayNext()
+        {
+            animations[queueIndex].Play();
+            queueIndex++;
+        }
+    }
+}
